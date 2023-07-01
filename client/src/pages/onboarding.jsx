8a9dc187e23dbfa4520c04601ed007f48ff392a9
Jayoooppp <patel.js@somaiya.edu>
2023-07-02 03:01:31 +0530

    Auth Changes

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,14 +1,67 @@
 import Avatar from "@/components/common/Avatar";
 import Input from "@/components/common/Input";
 import { useStateProvier } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import { ONBOARD_USER } from "@/utils/ApiRoutes";
+import axios from "axios";
 import Image from "next/image";
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 function onboarding() {
-  const [{ userInfo }] = useStateProvier();
+  const router = useRouter();
+  const [{ userInfo, newUser }, dispatch] = useStateProvier();
   const [name, setName] = useState(userInfo?.name || "")
   const [about, setAbout] = useState("");
   const [image, setImage] = useState("/default_avatar.png")
+
+  useEffect(() => {
+    if (!newUser && !userInfo?.email) {
+      router.push("/login")
+    } else if (!newUser) {
+      router.push("/");
+    }
+  }, [userInfo, newUser, router])
+
+  const onBoardUser = async () => {
+    if (validateDetails()) {
+      const email = userInfo.email;
+      const { data } = await axios.post(ONBOARD_USER, {
+        email,
+        name,
+        about,
+        image
+      })
+      if (data.status) {
+        dispatch({ type: reducerCases.SET_NEW_USER, newUser: false })
+        dispatch({
+          type: reducerCases.SET_NEW_USER,
+          userInfo: {
+            id: data.id,
+            name,
+            email,
+            profileImage: image,
+            status: about
+          }
+
+        })
+        router.push("/")
+      }
+      try {
+
+      } catch (error) {
+        console.log("🚀 ~ file: onboarding.jsx:20 ~ onBoardUser ~ error:", error)
+      }
+    }
+
+  }
+  const validateDetails = () => {
+    if (name.length < 3) {
+      return false;
+    }
+    return true;
+  }
+
   return (
     <div className="bg-panel-header-background h-screen w-screen flex flex-col items-center justify-center text-white">
       <div className="flex items-center justify-center gap-2">
@@ -20,6 +73,13 @@ function onboarding() {
         <div className="flex flex-col items-center justify-center mt-5 gap-6">
           <Input name="Display Name" state={name} setState={setName} label />
           <Input name="About" state={about} setState={setAbout} label />
+          <div className="flex items-center justify-center">
+            <button
+              onClick={onBoardUser}
+              className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
+            >Create Porfile</button>
+          </div>
+
         </div>
         <Avatar type="xl" image={image} setImage={setImage} />
       </div>
--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -3,3 +3,4 @@ const HOST = "http://localhost:5050";
 const AUTH_ROUTE = `${HOST}/api/auth`;
 
 export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
+export const ONBOARD_USER = `${AUTH_ROUTE}/onboard-user`;
