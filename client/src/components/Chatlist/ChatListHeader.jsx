2a349725c2dad1bfc3c6e8be3366d5b742b6171b
Jayoooppp <patel.js@somaiya.edu>
2023-07-05 00:37:20 +0530

    Auth fixed

--- client/src/components/Chatlist/ChatListHeader.jsx
+++ client/src/components/Chatlist/ChatListHeader.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Avatar from "../common/Avatar";
+import { useStateProvier } from "@/context/StateContext";
 
 function ChatListHeader() {
-  return <div>ChatListHeader</div>;
+  const [{ userInfo }, dispatch] = useStateProvier();
+
+  return (
+    <div div className="h-16 mx-4 py-3 flex justify-between items-center" >
+      <div className="cursor-pointer">
+        <Avatar type="sm" image={userInfo?.profileImage} />
+      </div>
+      <div>
+
+      </div>
+
+    </div>
+  )
 }
 
 export default ChatListHeader;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ChatList from "./Chatlist/ChatList";
+import Empty from "./Empty";
+import { onAuthStateChanged } from "firebase/auth";
+import { fireBaseAuth } from "@/utils/FirebaseConfig";
+import { async } from "@firebase/util";
+import { useRouter } from "next/router";
+import { useStateProvier } from "@/context/StateContext";
+import axios from "axios";
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
+import { reducerCases } from "@/context/constants";
 
 function Main() {
-  return <div>Main</div>;
+  const [redirectLogin, setRedirectLogin] = useState(false);
+  const router = useRouter();
+  const [{ userInfo }, dispatch] = useStateProvier();
+  console.log(userInfo)
+  useEffect(() => {
+    if (redirectLogin) {
+      router.push("/login");
+    }
+  }, [redirectLogin])
+  onAuthStateChanged(fireBaseAuth, async (currentUser) => {
+    if (!currentUser) {
+      console.log(currentUser)
+      setRedirectLogin(true);
+    }
+    if (!userInfo && currentUser?.email) {
+      console.log()
+      const { data } = await axios.post(CHECK_USER_ROUTE, {
+        email: currentUser?.email
+      });
+      if (!data.status) {
+        router.push("/login");
+      }
+      if (data?.data) {
+        const { id, name, email, profilePicture: profileImage, status } = data.data;
+        dispatch({
+          type: reducerCases.SET_USER_INFO,
+          userInfo: {
+            id, name, email, profileImage, status
+          }
+        })
+      }
+    }
+  })
+  return (
+    <>
+      <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
+        <ChatList />
+        <Empty />
+      </div>
+    </>
+  );
 }
 
 export default Main;
