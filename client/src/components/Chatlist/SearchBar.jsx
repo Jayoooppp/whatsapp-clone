a1c538a9c1d61a39eef55fab8e337e9c8276af6b
Jayoooppp <patel.js@somaiya.edu>
2023-07-15 01:01:51 +0530

    user search

--- client/src/components/Chatlist/SearchBar.jsx
+++ client/src/components/Chatlist/SearchBar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { useStateProvier } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import React, { useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi"
 import { BsFilter } from "react-icons/bs"
 function SearchBar() {
+  const [{ contactSearch }, dispatch] = useStateProvier();
+
   return (
     <div className="text-search-input-container-background-background flex py-3 pl-5 items-center gap-3 h-14 ">
       <div className="bg-panel-header-background flex items-center gap-3 px-3 py-1 rounded-lg flex-grow">
@@ -12,7 +16,15 @@ function SearchBar() {
           <input
             type="text"
             placeholder="Search or start a new chat"
-            className="bg-transparent text-sm focus:outline-none text-white w-full" />
+            className="bg-transparent text-sm focus:outline-none text-white w-full"
+            onChange={(e) => {
+              dispatch({
+                type: reducerCases.SET_CONTACT_SEARCH,
+                contactSearch: e.target.value
+              })
+            }}
+
+          />
         </div>
 
       </div>
